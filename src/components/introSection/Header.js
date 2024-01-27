936ca7a6fcc8433fc586444013d69c94d8d522f2
Aman Dhanda <amandhanda1978@gmail.com>
2024-01-27 16:51:52 +0530

    change some styling

--- src/components/introSection/Header.js
+++ src/components/introSection/Header.js
@@ -41,10 +41,10 @@ const Header = () => {
     className='bg-custom-background backdrop-blur fixed z-10 left-0  right-0 top-0 text-white shadow-slate-800 shadow-2xl'>
 
     <div className='w-full max-w-[1200px] mx-auto py-[10px] flex items-center justify-between   px-3 '>
-      <img className='w-[149px] h-[50px] cursor-pointer' src="/assets/movix-logo.svg" onClick={() =>{navigate('/')}} alt="hehe"/>
-      <div className='flex items-center gap-x-5 '>
-        <span className='cursor-pointer text-[20px]' onClick={click1}>Movies</span>
-        <span className='cursor-pointer text-[20px]' onClick = {click2}>TVshows</span>
+      <img className='w-[80px] h-[45px] lg:w-[149px] lg:h-[50px] cursor-pointer' src="/assets/movix-logo.svg" onClick={() =>{navigate('/')}} alt="hehe"/>
+      <div className='flex items-center gap-x-1 lg:gap-x-5 '>
+        <span className='cursor-pointer text-[12px] lg:text-[20px]' onClick={click1}>Movies</span>
+        <span className='cursor-pointer text-[12px] lg:text-[20px]' onClick = {click2}>TVshows</span>
         <div className='flex gap-x-1'>
 
         <input type="search" placeholder='Search' className='Searchbar h-[30px] pl-4 rounded-sm focus:transform scale-120 text-black'  onChange={(e) =>setMovieName(e.target.value)} />
